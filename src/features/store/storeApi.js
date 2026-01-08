24949e03752ec90248d47dc4913e6c360926d7e7
Logan Lessen <loganlessen@gmail.com>
2026-01-08 14:04:52 -0700

    update zarrita

--- src/features/store/storeApi.js
+++ src/features/store/storeApi.js
@@ -1,22 +1,25 @@
 import * as zarr from 'zarrita'
 
-export const fetchStoreShape = (ship, cruise, sensor) => {
+export const fetchStoreShape = async (ship, cruise, sensor) => {
     // Interpolate the base url with the actual thing we want to grab
     const url =
-        `https://noaa-wcsd-zarr-pds.s3.amazonaws.com/level_2/${ship}/${cruise}/${sensor}/${cruise}.zarr/`
+        `https://noaa-wcsd-zarr-pds.s3.amazonaws.com/level_5/${ship}/${cruise}/${sensor}/${cruise}_resampled.zarr/level_0`
 
     // This big promise chain fetches the zarr store at the given url and then unwraps it to get the data we want
     // Each one will be slightly different, so the code can't really be reused
-    return zarr.withConsolidated(new zarr.FetchStore(url)).then((storePromise) => {
-        const zarrGroup = zarr.open.v2(storePromise, { kind: "group" })
-        return zarrGroup
-    }).then((rootPromise) => {
-        // Here the root store's promise is resolved and we tell it what data we want to resolve
-        const svArray = zarr.open(rootPromise.resolve("Sv"), { kind: "array" })
-        return svArray
-    }).then((svArray) => {
-        // This fetcher gets the store's shape
-        // A new one will have to be created for each property to be fetched
-        return svArray.shape
-    })
+    const root = zarr.root(new zarr.FetchStore(url))
+
+    const arr = await zarr.open.v3(root.resolve("Sv"), { kind: 'array' })
+    return arr.shape
+}
+
+export const fetchStoreAttributes = async (ship, cruise, sensor) => {
+    const url =
+        `https://noaa-wcsd-zarr-pds.s3.amazonaws.com/level_5/${ship}/${cruise}/${sensor}/${cruise}_resampled.zarr/level_0`;
+
+    const root = zarr.root(new zarr.FetchStore(url))
+
+    const rootPromise = await zarr.open.v3(root, { kind: 'group' })
+    return rootPromise.attrs
+
 }
--- src/features/store/storeSlice.js
+++ src/features/store/storeSlice.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { fetchStoreShape } from './zarrApi.js';
+import { fetchStoreShape, fetchStoreAttributes } from './storeApi.js';
 
 // Prevent a bunch of null values initially
 const initialState = {
@@ -12,33 +12,58 @@ const initialState = {
 
 // The selector is just a getter for the data we want
 // Each property we want will need a selector for it
-export const selectStoreShape = (state) => state.zarr.storeShape
+export const selectStoreAttributes = (state) => state.store.storeAttributes
+export const selectStoreShape = (state) => state.store.storeShape
+export const selectCruise = (state) => state.store.cruise
+export const selectSvMin = (state) => state.store.svMin
+export const selectSvMax = (state) => state.store.svMax
+export const selectFrequencyIndex = (state) => state.store.frequenctIndex
+export const selectColorIndex = (state) => state.store.colorIndex
 
 // Eventually there will be a thunk for each property that needs to be accessed
 // The thunks are separate from the Api in order to make each section more readable
 export const storeShapeAsync = createAsyncThunk(
-    "zarr/fetchStoreShape",
+    "store/fetchStoreShape",
     async ({ ship, cruise, sensor }) => {
         const response = fetchStoreShape(ship, cruise, sensor)
         return response
     }
 )
 
-export const zarrSlice = createSlice({
-    name: "zarr",
+export const storeAttributesAsync = createAsyncThunk(
+    "store/fetchStoreAttributes",
+    async ({ ship, cruise, sensor }) => {
+        const response = fetchStoreAttributes(ship, cruise, sensor)
+        return [...response.data].map(x => Number(x))
+    }
+)
+
+export const storeSlice = createSlice({
+    name: "store",
     initialState,
     reducers: {},
     // Handle the state of the thunk, and add it to the state
     extraReducers: builder => {
-        builder.addCase(storeShapeAsync.pending, state => {
-            state.storeShapeStatus = "loading"
-        }).addCase(storeShapeAsync.fulfilled, (state, action) => {
-            state.storeShapeStatus = "idle"
-            state.storeShape = action.payload
-        }).addCase(storeShapeAsync.rejected, state => {
-            state.storeShapeStatus = "failed"
-        })
+        builder
+            // Store attributes
+            .addCase(storeAttributesAsync.pending, state => {
+                state.storeAttributesStatus = "loading";
+            }).addCase(storeAttributesAsync.fulfilled, (state, action) => {
+                state.storeAttributesStatus = "idle";
+                state.storeAttributes = action.payload; // attrs
+            }).addCase(storeAttributesAsync.rejected, state => {
+                state.storeAttributesStatus = "failed";
+            })
+            // Store shape
+            .addCase(storeShapeAsync.pending, state => {
+                state.storeShapeStatus = "loading"
+            }).addCase(storeShapeAsync.fulfilled, (state, action) => {
+                state.storeShapeStatus = "idle"
+                state.storeShape = action.payload
+            }).addCase(storeShapeAsync.rejected, state => {
+                state.storeShapeStatus = "failed"
+            })
     }
 })
 
-export default zarrSlice.reducer
+export default storeSlice.reducer
