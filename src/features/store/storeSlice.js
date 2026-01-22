0e930be93f7e0b35720ee9bd896eca50c90b899f
Logan Lessen <loganlessen@gmail.com>
2026-01-22 13:07:04 -0700

    add dynamic loading (VERY buggy)

--- src/features/store/storeSlice.js
+++ src/features/store/storeSlice.js
@@ -28,37 +28,11 @@ export const storeShapeAsync = createAsyncThunk(
     }
 )
 
-export const storeAttributesAsync = createAsyncThunk(
-    "store/fetchStoreAttributes",
-    async ({ ship, cruise, sensor }) => {
-        const response = fetchStoreAttributes(ship, cruise, sensor)
-        return [...response.data].map(x => Number(x))
-    }
-)
-
 export const svAsync = createAsyncThunk(
     "store/fetchSv",
     async ({ ship, cruise, sensor, indexDepth, indexTime }) => {
         const response = fetchSv(ship, cruise, sensor, indexDepth, indexTime)
         return [...response.data].map(x => Number(Math.round(x * 1e2) / 1e2))
-    }
-)
-
-export const depthAsync = createAsyncThunk(
-    "store/fetchDepth",
-    async ({ ship, cruise, sensor, indexDepth }) => {
-        const response = await fetchDepth(ship, cruise, sensor, indexDepth)
-        // Round to 2 decimal places
-        return Math.round(response * 1e2) / 1e2
-    }
-)
-
-export const depthArrayAsync = createAsyncThunk(
-    "store/fetchDepthArray",
-    async ({ ship, cruise, sensor }) => {
-        const response = await fetchDepthArray(ship, cruise, sensor)
-        // Round to 2 decimal places
-        return response.data
     }
 )
 
@@ -69,15 +43,6 @@ export const storeSlice = createSlice({
     // Handle the state of the thunk, and add it to the state
     extraReducers: builder => {
         builder
-            // Store attributes
-            .addCase(storeAttributesAsync.pending, state => {
-                state.storeAttributesStatus = "loading";
-            }).addCase(storeAttributesAsync.fulfilled, (state, action) => {
-                state.storeAttributesStatus = "idle";
-                state.storeAttributes = action.payload; // attrs
-            }).addCase(storeAttributesAsync.rejected, state => {
-                state.storeAttributesStatus = "failed";
-            })
             // Store shape
             .addCase(storeShapeAsync.pending, state => {
                 state.storeShapeStatus = "loading";
@@ -95,24 +60,6 @@ export const storeSlice = createSlice({
                 state.sv = action.payload;
             }).addCase(svAsync.rejected, state => {
                 state.svStatus = "failed";
-            })
-            // Depth
-            .addCase(depthAsync.pending, state => {
-                state.depthStatus = "loading";
-            }).addCase(depthAsync.fulfilled, (state, action) => {
-                state.depthStatus = "idle";
-                state.depth = action.payload;
-            }).addCase(depthAsync.rejected, state => {
-                state.depthStatus = "failed";
-            })
-            // DepthArray
-            .addCase(depthArrayAsync.pending, state => {
-                state.depthArrayStatus = "loading";
-            }).addCase(depthArrayAsync.fulfilled, (state, action) => {
-                state.depthArrayStatus = "idle";
-                state.depthArray = action.payload;
-            }).addCase(depthArrayAsync.rejected, state => {
-                state.depthArrayStatus = "failed";
             })
     }
 })
--- src/pages/Echofish.jsx
+++ src/pages/Echofish.jsx
@@ -1,14 +1,15 @@
 import { useRef, useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import * as THREE from 'three'
-import { scaleSequential } from  'd3-scale'
+import { scaleSequential } from 'd3-scale'
 import { interpolateTurbo } from 'd3-scale-chromatic'
-import { Canvas } from '@react-three/fiber'
+import { Canvas, useFrame, useThree } from '@react-three/fiber'
 import { MapControls } from '@react-three/drei'
 import { fetchSvTile } from '../features/store/storeApi'
+import { selectStoreShape, storeShapeAsync } from '../features/store/storeSlice.js'
 import '../App.css'
-import { data } from 'react-router'
 
-const Plane = (props) => {
+const Tile = ({ coords, tileSize, storeShape }) => {
     const meshRef = useRef(null);
     const [texture, setTexture] = useState(null);
     // const dispatch = useDispatch()
@@ -20,7 +21,34 @@ const Plane = (props) => {
             const cruise = "HB1906"
             const sensor = "EK60"
 
-            const initTile = await fetchSvTile(ship, cruise, sensor, 0, 256, 0, 256)
+            const dataDimension = storeShape;
+
+            const maxBoundsValue =
+                [[-1 * Math.ceil(dataDimension[0] / tileSize) * tileSize, 0], [0, Math.ceil(dataDimension[1] / tileSize) * tileSize]];
+            const maxTileBoundsX = Math.abs(maxBoundsValue[1][1]) / tileSize;
+            const maxTileBoundsY = Math.abs(maxBoundsValue[0][0]) / tileSize;
+
+            // TODO: Do something when we go beyond the edge of the data
+            if (coords.y < 0 || coords.y >= maxTileBoundsY || coords.x < 0 || coords.x >= maxTileBoundsX) return
+
+            const maxBoundsY = Math.abs(dataDimension[0]);
+            const maxBoundsX = Math.abs(dataDimension[1]);
+
+            const indicesLeft = tileSize * coords.x;
+            const indicesRight = Math.min(tileSize * coords.x + tileSize, maxBoundsX);
+            const indicesTop = tileSize * coords.y;
+            const indicesBottom = Math.min(tileSize * coords.y + tileSize, maxBoundsY);
+
+            // FIX: This occasionally tries to fetch data that does not exist
+            const initTile = await fetchSvTile(
+                ship,
+                cruise,
+                sensor,
+                indicesTop,
+                indicesBottom,
+                indicesLeft,
+                indicesRight
+            )
             const tile = initTile;
 
             // Maps the SV points to a d3 scale
@@ -38,22 +66,85 @@ const Plane = (props) => {
             }
 
             const dataTexture = new THREE.DataTexture(colorData, 256, 256, THREE.RGBAFormat, THREE.UnsignedByteType);
+
+            // WARN: This is deprecated?
             dataTexture.flipY = true;
+
             dataTexture.needsUpdate = true;
 
             setTexture(dataTexture);
+            return;
         }
 
         makeTile();
-    }, [])
+    }, [coords, tileSize]);
 
     return (
         <mesh
-            {...props}
+            position={[coords.x, coords.y, 0]}
             ref={meshRef}>
             <planeGeometry args={[1, 1]} />
             {texture && <meshStandardMaterial map={texture} />}
         </mesh>
+    )
+}
+
+const tileCache = new Map()
+const fetchTile = ({ coords, ...props }) => {
+    const key = `${coords.x},${coords.y}`;
+    if (!tileCache.has(key)) {
+        tileCache.set(key, (<Tile key={key} coords={coords} {...props} />));
+    }
+
+    return tileCache.get(key);
+}
+
+const TileMap = () => {
+    const { camera, viewport } = useThree();
+    const [tiles, setTiles] = useState([]);
+    const dispatch = useDispatch()
+
+    const storeShape = useSelector(selectStoreShape);
+
+    useEffect(() => {
+        // Temporary values
+        const ship = "Henry_B._Bigelow"
+        const cruise = "HB1906"
+        const sensor = "EK60"
+        // Dispatch the thunk call so it goes through the store
+        dispatch(storeShapeAsync({ ship, cruise, sensor }))
+    }, [dispatch])
+
+    useFrame(() => {
+        if (!storeShape) return
+
+        // PERF: Panning is incredibly slow, especially trying to pan past the bounds of the data
+        const left = camera.position.x - viewport.width / 2;
+        const right = camera.position.x + viewport.width / 2;
+        const bottom = camera.position.y - viewport.height / 2;
+        const top = camera.position.y + viewport.height / 2;
+
+        const TILE_PADDING = 0;
+        const minX = Math.floor(left) - TILE_PADDING;
+        const maxX = Math.floor(right) + TILE_PADDING;
+        const minY = Math.floor(bottom) - TILE_PADDING;
+        const maxY = Math.floor(top) + TILE_PADDING;
+
+        const newTiles = [];
+
+        for (let x = minX; x < maxX; x++) {
+            for (let y = minY; y < maxY; y++) {
+                newTiles.push(fetchTile({ coords: { x: x, y: y }, tileSize: 256, storeShape: storeShape }));
+            }
+        }
+
+        setTiles(newTiles);
+    })
+
+    return (
+        <>
+            {tiles}
+        </>
     )
 }
 
@@ -65,13 +156,13 @@ const Echofish = () => {
             style={{ width: '100vw', height: '100vh' }}>
             {/* Some lighting */}
             <ambientLight intensity={Math.PI / 2} />
-            <Plane position={[0, 0, 0]} />
+            <TileMap />
             {/* Enables panning and zooming on the box */}
             <MapControls
                 makeDefault
                 enableRotate={false}
                 minDistance={0.25}
-                maxDistance={10}
+                maxDistance={1}
                 maxPolarAngle={Math.PI / 2}
                 screenSpacePanning={true}
             />
