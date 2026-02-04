4138149c824011c63b308ecf6d5ab53bce0aba56
Logan Lessen <loganlessen@gmail.com>
2026-02-04 14:43:16 -0700

    It's red now and laggier

--- src/pages/Echofish.jsx
+++ src/pages/Echofish.jsx
@@ -9,90 +9,97 @@ import { fetchSvTile } from '../features/store/storeApi'
 import { selectStoreShape, storeShapeAsync } from '../features/store/storeSlice.js'
 import '../App.css'
 
-const Tile = ({ coords, tileSize, storeShape, ...props }) => {
-    const meshRef = useRef(null);
-    const [texture, setTexture] = useState(null);
+const fetchTexture = async (coords, tileSize, storeShape) => {
+    const ship = "Henry_B._Bigelow"
+    const cruise = "HB1906"
+    const sensor = "EK60"
+
+    const dataDimension = storeShape;
+
+    const maxBoundsValue =
+        [[-1 * Math.ceil(dataDimension[0] / tileSize) * tileSize, 0], [0, Math.ceil(dataDimension[1] / tileSize) * tileSize]];
+    const maxTileBoundsX = Math.abs(maxBoundsValue[1][1]) / tileSize;
+    const maxTileBoundsY = Math.abs(maxBoundsValue[0][0]) / tileSize;
+
+    // TODO: Do something when we go beyond the edge of the data
+    if (coords.y < 0 || coords.y >= maxTileBoundsY || coords.x < 0 || coords.x >= maxTileBoundsX) return;
+
+    const maxBoundsY = Math.abs(dataDimension[0]);
+    const maxBoundsX = Math.abs(dataDimension[1]);
+
+    const indicesLeft = tileSize * coords.x;
+    const indicesRight = Math.min(tileSize * coords.x + tileSize, maxBoundsX);
+    const indicesTop = tileSize * coords.y;
+    const indicesBottom = Math.min(tileSize * coords.y + tileSize, maxBoundsY);
+
+    // FIX: This occasionally tries to fetch data that does not exist
+    // and loads the entire dataset at once instead of by tile
+    const initTile = await fetchSvTile(
+        ship,
+        cruise,
+        sensor,
+        indicesTop,
+        indicesBottom,
+        indicesLeft,
+        indicesRight
+    )
+    const tile = initTile;
+
+    const [width, height] = tile.shape;
+
+    // Maps the SV points to a d3 scale
+    const colorScale = scaleSequential(interpolateTurbo).domain([-100, 0]);
+    // This array stores each byte of color separately as RGBA
+    const colorData = new Uint8ClampedArray(tile.data.length * 4).fill(255);
+
+    for (let i = 0; i < tile.data.length; i++) {
+        const sv = tile.data[i];
+        const color = new THREE.Color(colorScale(sv));
+        colorData[i * 4 + 0] = Math.round(color.r * 255);
+        colorData[i * 4 + 1] = Math.round(color.g * 255);
+        colorData[i * 4 + 2] = Math.round(color.b * 255);
+    }
+
+    const dataTexture = new THREE.DataTexture(colorData, width, height, THREE.RGBAFormat, THREE.UnsignedByteType);
+
+    // WARN: This is deprecated?
+    dataTexture.flipY = true;
+
+    dataTexture.needsUpdate = true;
+
+    return dataTexture;
+}
+
+const Tile = ({ coords, dataTexture, ...props }) => {
+    const meshRef = useRef();
+    const materialRef = useRef();
 
     useEffect(() => {
-        const makeTile = async () => {
-            const ship = "Henry_B._Bigelow"
-            const cruise = "HB1906"
-            const sensor = "EK60"
+        if (!meshRef.current) return;
 
-            const dataDimension = storeShape;
+        meshRef.current.position.set(coords.x, coords.y, 0);
+    }, [coords])
 
-            const maxBoundsValue =
-                [[-1 * Math.ceil(dataDimension[0] / tileSize) * tileSize, 0], [0, Math.ceil(dataDimension[1] / tileSize) * tileSize]];
-            const maxTileBoundsX = Math.abs(maxBoundsValue[1][1]) / tileSize;
-            const maxTileBoundsY = Math.abs(maxBoundsValue[0][0]) / tileSize;
+    useEffect(() => {
+        if (!materialRef.current || !dataTexture) return;
 
-            // TODO: Do something when we go beyond the edge of the data
-            if (coords.y < 0 || coords.y >= maxTileBoundsY || coords.x < 0 || coords.x >= maxTileBoundsX) return (<></>)
-
-            const maxBoundsY = Math.abs(dataDimension[0]);
-            const maxBoundsX = Math.abs(dataDimension[1]);
-
-            const indicesLeft = tileSize * coords.x;
-            const indicesRight = Math.min(tileSize * coords.x + tileSize, maxBoundsX);
-            const indicesTop = tileSize * coords.y;
-            const indicesBottom = Math.min(tileSize * coords.y + tileSize, maxBoundsY);
-
-            // FIX: This occasionally tries to fetch data that does not exist
-            // and loads the entire dataset at once instead of by tile
-            const initTile = await fetchSvTile(
-                ship,
-                cruise,
-                sensor,
-                indicesTop,
-                indicesBottom,
-                indicesLeft,
-                indicesRight
-            )
-            const tile = initTile;
-
-            const [width, height] = tile.shape;
-
-            // Maps the SV points to a d3 scale
-            const colorScale = scaleSequential(interpolateTurbo).domain([-100, 0]);
-            // This array stores each byte of color separately as RGBA
-            const colorData = new Uint8ClampedArray(tile.data.length * 4).fill(255);
-
-            for (let i = 0; i < tile.data.length; i++) {
-                const sv = tile.data[i];
-                const color = new THREE.Color(colorScale(sv));
-                colorData[i * 4 + 0] = Math.round(color.r * 255);
-                colorData[i * 4 + 1] = Math.round(color.g * 255);
-                colorData[i * 4 + 2] = Math.round(color.b * 255);
-            }
-
-            const dataTexture = new THREE.DataTexture(colorData, width, height, THREE.RGBAFormat, THREE.UnsignedByteType);
-
-            // WARN: This is deprecated?
-            dataTexture.flipY = true;
-
-            dataTexture.needsUpdate = true;
-
-            setTexture(dataTexture);
-            return;
-        }
-
-        makeTile();
-    }, [coords, tileSize]);
+        materialRef.current.map = dataTexture;
+        materialRef.current.needsUpdate = true;
+    }, [dataTexture])
 
     return (
-        <mesh
-            {...props}
-            position={[coords.x, coords.y, 0]}
-            ref={meshRef}>
+        <mesh ref={meshRef} {...props}>
             <planeGeometry args={[1, 1]} />
-            {texture && <meshStandardMaterial map={texture} />}
+            <meshStandardMaterial ref={materialRef} />
         </mesh>
     )
 }
 
 const TileMap = () => {
-    const [visibleKeys, setVisibleKeys] = useState([]);
-    const tiles = useRef(new Map());
+    const textures = useRef(new Map());
+    const tilePool = useRef([]);
+    const [tileData, setTileData] = useState([]);
+    const [bounds, setBounds] = useState();
     const dispatch = useDispatch();
 
     const storeShape = useSelector(selectStoreShape);
@@ -104,7 +111,49 @@ const TileMap = () => {
         const sensor = "EK60"
         // Dispatch the thunk call so it goes through the store
         dispatch(storeShapeAsync({ ship, cruise, sensor }))
-    }, [dispatch])
+    }, [dispatch]);
+
+    useEffect(() => {
+        const setTextures = async (tiles) => {
+            // Batch load all the textures asynchronously
+            // await Promise.all(
+            //     tiles.map(tile => (
+            //         fetchTexture(tile.coords, 256, storeShape).then(texture => {
+            //             textures.current.set(tile.key, texture);
+            //         })
+            //     ))
+            // );
+
+            for (let tile of tiles) {
+                const texture = await fetchTexture(tile.coords, 256, storeShape);
+                textures.current.set(tile.key, texture);
+            }
+
+            setTileData(prev => [...prev])
+        };
+
+        if (!storeShape) return;
+
+        const missingKeys = tileData.filter(tile => !tile.texture);
+        if (missingKeys.length === 0) return;
+
+        setTextures(missingKeys);
+    }, [tileData, storeShape]);
+
+    useEffect(() => {
+        if (!bounds) return;
+
+        const [minX, maxX, minY, maxY] = bounds.split(",").map(Number);
+
+        const newTileData = [];
+        for (let x = minX; x < maxX; x++) {
+            for (let y = minY; y < maxY; y++) {
+                const key = `${x},${y}`;
+                newTileData.push({ key: key, coords: { x, y }, texture: textures.current.get(key) });
+            }
+        }
+        setTileData(newTileData);
+    }, [bounds]);
 
     useFrame(({ camera, viewport }) => {
         if (!storeShape) return
@@ -120,37 +169,22 @@ const TileMap = () => {
         const minY = Math.floor(bottom) - TILE_PADDING;
         const maxY = Math.floor(top) + TILE_PADDING;
 
-        const newVisibleKeys = [];
-        for (let x = minX; x < maxX; x++) {
-            for (let y = minY; y < maxY; y++) {
-                // HACK: Store the key as a 32-bit number where the first
-                // 16 bits are the x and the second 16 are the y. The next best
-                // solution is to use a string, which does not have overflow
-                // problems but is significantly less space efficient
-                const key = (x << 16) | (y & 0xffff);
-                newVisibleKeys.push(key);
-            }
-        }
-        setVisibleKeys(newVisibleKeys);
+        const newBounds = `${minX},${maxX},${minY},${maxY}`;
+        setBounds(newBounds);
     })
 
-    visibleKeys.forEach(key => {
-        if (!tiles.current.has(key)) {
-            // Decode the key for the coordinates
-            const x = key >> 16;
-            const y = key & 0xffff;
-            tiles.current.set(key, (<Tile
-                key={key}
-                coords={{ x, y }}
-                tileSize={256}
-                storeShape={storeShape}
-            />))
-        }
-    })
-
+    console.log(tileData)
     return (
-        <>{Array.from(tiles.current.values())}</>
-    )
+        <>
+            {tileData.map(tile => (
+                <Tile
+                    key={tile.key}
+                    coords={tile.coords}
+                    dataTexture={tile.texture}
+                />)
+            )}
+        </>
+    );
 }
 
 const Echofish = () => {
